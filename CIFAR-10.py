75e9eed1a13b331c7a51da1f906256f3d6f36368
David Jo <tkdave44@gmail.com>
2024-09-11 23:38:10 -0700

    Add files via upload

---
+++ CIFAR-10.py
@@ -0,0 +1,6 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+#================================================= Data Loading ========================================================
+tf.keras.datasets.cifar10.load_data
