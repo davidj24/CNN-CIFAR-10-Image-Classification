9869e0c53fcb4d537eae336f26969a3cc7328bfe
David Jo <tkdave44@gmail.com>
2024-09-11 23:42:47 -0700

    Delete CIFAR-10.py

--- CIFAR-10.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-
-#================================================= Data Loading ========================================================
-tf.keras.datasets.cifar10.load_data
