597054d0602405ae67d0ed4e49ffecb7d3f8eb44
David Jo <tkdave44@gmail.com>
2024-09-17 19:01:32 -0700

    Got rid of final max pooling layer and dropout layers

--- CIFAR-10.py
+++ CIFAR-10.py
@@ -3,7 +3,6 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.datasets import cifar10
-from tensorflow.keras.layers import preprocessing
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -14,10 +13,19 @@ import pandas as pd
 
 # =============================================== Loading Data ===============================================
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
-print(np.unique(X_train))
+
+X_train = X_train / 255
+X_test = X_test / 255
 
 X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train,
                                                        test_size=0.2, random_state=0)
+
+classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+def show_image(dataset, labels, index):
+    plt.figure(figsize=(15, 2))
+    plt.imshow(dataset[index])
+    plt.xlabel(classes[labels[index][0]])
 
 
 # =============================================== Modeling =================================================
@@ -31,46 +39,46 @@ model = keras.Sequential([
     layers.BatchNormalization(),
     layers.Conv2D(filters=32, kernel_size=3, activation='relu', padding='same'),
     layers.MaxPool2D(),
-    layers.Dropout(.3),
+    # layers.Dropout(.3),
 
     layers.BatchNormalization(),
     layers.Conv2D(filters=64, kernel_size=3, activation='relu', padding='same'),
     layers.MaxPool2D(),
-    layers.Dropout(.3),
+    # layers.Dropout(.3),
 
     layers.BatchNormalization(),
     layers.Conv2D(filters=128, kernel_size=3, activation='relu', padding='same'),
     layers.MaxPool2D(),
-    layers.Dropout(.3),
+    # layers.Dropout(.3),
 
     layers.BatchNormalization(),
     layers.Conv2D(filters=256, kernel_size=3, activation='relu', padding='same'),
     layers.MaxPool2D(),
-    layers.Dropout(.3),
+    # layers.Dropout(.3),
 
     layers.BatchNormalization(),
     layers.Conv2D(filters=512, kernel_size=3, activation='relu', padding='same'),
     layers.MaxPool2D(),
-    layers.Dropout(.3),
+    # layers.Dropout(.3),
 
     layers.BatchNormalization(),
     layers.Conv2D(filters=1024, kernel_size=3, activation='relu', padding='same'),
     # layers.MaxPool2D(),
-    layers.Dropout(.3),
+    # layers.Dropout(.3),
 
     layers.BatchNormalization(),
     layers.Flatten(),
-    layers.Dense(256, activation='relu'),
-    layers.Dropout(.3),
+    layers.Dense(64, activation='relu'),
+    # layers.Dropout(.3),
     layers.Dense(10, activation='softmax')
-])
+    ])
     
 
 early_stopping = EarlyStopping(
     patience=4,
     min_delta=.001,
     restore_best_weights=True
-)
+    )
 
 model.compile(
     optimizer='adam',
@@ -101,5 +109,18 @@ print(f'loss: {loss}')
 print(f'accuracy: {accuracy}')
 
 # Current scores:
-loss: 0.7955142259597778
-accuracy: 0.7235999703407288
+# loss: 0.7955142259597778
+# accuracy: 0.7235999703407288
+
+
+#Following with video 3 total layers
+# loss: 0.8620476722717285
+# accuracy: 0.708899974822998
+
+# With Data Augmentation 3 total layers
+# loss: 0.7880503535270691
+# accuracy: 0.7373999953269958
+
+# Dropout layers with only 3 total and 64 neurons in the head dense
+# loss: 0.7983840107917786
+# accuracy: 0.7282000184059143
