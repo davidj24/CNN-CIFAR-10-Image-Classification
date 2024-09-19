b2653edb67af290098fd959b64fca3e8228bad12
David Jo <tkdave44@gmail.com>
2024-09-19 13:14:17 -0700

    Saved best params

---
+++ CIFAR-10_w_optuna.py
@@ -0,0 +1,11 @@
+# Best params!!!
+#  {'num_conv_layers': 4,
+# 'filters_layer_0': 512,
+# 'dropout_layer_0': 0.2924515218223497, 
+# 'filters_layer_1': 1024, 
+# 'dropout_layer_1': 0.20420635555557912, 
+# 'filters_layer_2': 1024, 
+# 'dropout_layer_2': 0.4938435933532547, 
+# 'filters_layer_3': 512, 
+# 'dropout_layer_3': 0.477406309189756, 
+# 'num_dense_neurons': 342, 'dense_dropout_3': 0.49155865824030837}
